63be00356c91d9c6de7426f67bf24739dd410005
hkdnet <satoko.itse@gmail.com>
2019-01-15 01:04:32 +0900

    Show input of NG cases

--- test.sh
+++ test.sh
@@ -13,7 +13,7 @@ try() {
   if [ "$actual" = "$expected" ]; then
     echo "$input => $actual"
   else
-    echo "$expected expected, but got $actual"
+    echo "$input => NG: $expected expected, but got $actual"
     exit 1
   fi
 }
