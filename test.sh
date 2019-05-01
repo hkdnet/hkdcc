c5510d0170f4aa8b095bf1bd319bb07cdd662226
hkdnet <satoko.itse@gmail.com>
2019-05-01 18:00:36 +0900

    Add test cases for unary operators

--- test.sh
+++ test.sh
@@ -22,6 +22,10 @@ try() {
 try 0 'main() { return 0; }'
 try 42 'main() { return 42; }'
 
+# unary operator
+try +1 'main() { return +1; }'
+try -1 'main() { return -1; }'
+
 # add
 try 3 'main() { return 1+2; }'
 try 13 'main() { return 1+10+2; }'
